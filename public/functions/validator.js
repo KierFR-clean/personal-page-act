fea97d42fc237a7c70b495d826451f75d3c6c610
Kier <kierchristian64@gmail.com>
2024-09-29 18:10:48 +0800

    will update later

---
+++ public/functions/validator.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+    $('#email').on('input', function() {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (emailRegex.test($(this).val())) {
+        $('#emailFeedback').text(''); 
+      } else {
+        $('#emailFeedback').text('Please enter a valid email address.');
+      }
+    });
+
+    function emailValidator() {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      const email = $('email').val();
+      return emailRegex.test(email);
+    }
+
+
+    function valCntForm(event) {
+        event.preventDefault();
+        const name = $('#name').val().trim();
+        const email = $('#email').val().trim();
+        const message = $('#message').val().trim();
+
+       
+      if (name === '' || email === '' || message === '') {
+          swal({
+            title: "Error!",
+            text: "Please fill all fields!",
+            icon: "error",
+            button: "OK",
+        });
+            return;
+        }  else if (!emailValidator()) {
+          swal({
+          title: "Error!",
+          text: "Please enter a valid email!",
+          icon: "error",
+          button: "OK",
+        });
+          return;
+
+      }
+
+        swal({
+            title: "Success!",
+            text: "Message submitted successfully!",
+            icon: "success",
+            button: "OK",
+        }).then((value) => {
+           
+            $('#contact-form')[0].reset();
+        });
+    }
+
+    $('#contact-form').on('submit', valCntForm);
+  });
+
+
+  
